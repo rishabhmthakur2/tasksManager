ee0c44d20e1fb9de238bd40a6f0b2f22d02d970c
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-06-07 14:34:55 +0530

    Completed user authentication for routes

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -18,19 +18,7 @@ router.get("/users/me", auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) {
-            return res.status(404).send("No user with that id found");
-        }
-        return res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpadtes = ['name','password','email','age'];
     const isValidUpdate = updates.every((update) => allowedUpadtes.includes(update));
@@ -42,27 +30,18 @@ router.patch("/users/:id", async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id);
-        updates.forEach((update)=> user[update] = req.body[update]);
-
-        await user.save();
-
-        if(!user){
-            return res.status(404).send("No user with that id found");
-        }
-        res.send(user);
+        updates.forEach((update)=> req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user);
     } catch (error) {
         res.status(400).send(error);
     }
 });
 
-router.delete("/users/:id", async (req, res)=>{
+router.delete("/users/me", auth, async (req, res)=>{
     try{
-        const user = await User.findByCredentials(req.params.id);
-        if(!user){
-            res.status(400).send("No user found with that id");
-        }
-        res.send(user);
+        await req.user.remove();
+        res.send(req.user);
     } catch(error){
         res.status(500).send(error);
     }
